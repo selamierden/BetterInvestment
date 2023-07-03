0d75e88f1a293d0780b45669d47859d6a9aa05ae
selamierdn <erdenselami07@gmail.com>
2023-07-03 22:31:50 +0300

    ok

--- futures.js
+++ futures.js
@@ -58,6 +58,18 @@ async function submitForm() {
     actionCell.innerHTML = '<button onclick="deleteRow(this)" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>';
     refreshCell.innerHTML = '<button onclick="refreshRow(this)" id="rbtn" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i></button>';
 
+    if (profitRate > 0) {
+      profitRateCell.style.color = "green";
+    } else if (profitRate < 0) {
+      profitRateCell.style.color = "red";
+    }
+
+    if (profit > 0) {
+      profitCell.style.color = "green";
+    } else if (profit < 0) {
+      profitCell.style.color = "red";
+    }
+
 };
 
 // Define a function to update the current price for a row
@@ -101,7 +113,7 @@ function deleteRow(button) {
   // Get the row that the button is in
   var row = button.parentNode.parentNode;
   
-  // Get the spot data from the row cells
+  // Get the trade data from the row cells
   var coin = row.cells[0].innerHTML;
   var amount = row.cells[1].innerHTML;
   var leverage = row.cells[2].innerHTML;
@@ -111,7 +123,7 @@ function deleteRow(button) {
   // Remove the row from the table
   row.parentNode.removeChild(row);
   
-  // Remove the spot data from localStorage
+  // Remove the trade data from localStorage
   var trades = JSON.parse(localStorage.getItem("trades"));
   var index = -1;
   for (var i = 0; i < trades.length; i++) {
@@ -126,67 +138,66 @@ function deleteRow(button) {
   }
   };
 
-// sayfa yüklendiğinde çalışacak fonksiyon
-window.onload = function() {
-  // Retrieve the trades from localStorage
-  var trades = JSON.parse(localStorage.getItem("trades"));
-
-  // If there are trades in localStorage, add them to the table
-  if (trades !== null && trades.length > 0) {
-    var table = document.getElementById("trade-table");
-
-    for (var i = 0; i < trades.length; i++) {
-      var trade = trades[i];
-
-      var row = table.insertRow(-1);
-      var coinCell = row.insertCell(0);
-      var leverageCell = row.insertCell(1);
-      var entryPriceCell = row.insertCell(2);
-      var amountCell = row.insertCell(3);
-      var actionCell = row.insertCell(4);
-      var refreshCell = row.insertCell(5);
-
-      coinCell.innerHTML = trade.coin;
-      leverageCell.innerHTML = trade.leverage;
-      entryPriceCell.innerHTML = trade.entryPrice;
-      amountCell.innerHTML = trade.amount;
-
-      actionCell.innerHTML = '<button onclick="deleteRow(this)" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>';
-      refreshCell.innerHTML = '<button onclick="refreshRow(this)" id="rbtn" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i></button>';
-
-    }
-  }
-
-  // Get the latest prices from Coingecko and update the table
-  var rows = document.querySelectorAll('#trade-table tr:not(:first-child)');
-  for (var i = 0; i < rows.length; i++) {
-    var coin = rows[i].cells[0].innerHTML;
-    var amount = rows[i].cells[1].innerHTML;
-    var leverage = rows[i].cells[2].innerHTML;
-    var entryPrice = rows[i].cells[3].innerHTML;
-    var currentPriceCell = rows[i].insertCell(4);
-    var profitCell = rows[i].insertCell(5);
-    var profitRateCell = rows[i].insertCell(6);
-    
-    fetch('https://api.coingecko.com/api/v3/simple/price?ids=' + coin.toLowerCase() + '&vs_currencies=usd')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var currentPrice = data[coin.toLowerCase()].usd;
-      var profit = ((currentPrice / entryPrice) * amount - amount)*leverage;
-      var profitRate = (profit / amount) * 100;
-      currentPriceCell.innerHTML = currentPrice;
-      profitCell.innerHTML = profit.toFixed(2);
-      profitRateCell.innerHTML = profitRate.toFixed(2) + "%";
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-  }
-}
-
+  //
+  window.onload = async function() {
+    // Get the spots data from localStorage and populate the table
+    var trades = JSON.parse(localStorage.getItem("trades"));
+    if (trades !== null) {
+      var table = document.getElementById("trade-table");
+      for (var i = 0; i < trades.length; i++) {
+        var row = table.insertRow(-1);
+        var coinCell = row.insertCell(0);
+        var amountCell = row.insertCell(1);
+        var leverageCell = row.insertCell(2)
+        var entryPriceCell = row.insertCell(3)
+        var currentpriceCell = row.insertCell(4);
+        var profitCell = row.insertCell(5);
+        var profitRateCell = row.insertCell(6);
+        var actionCell = row.insertCell(7);
+        var refreshCell = row.insertCell(8);
   
+        coinCell.innerHTML = trades[i].coin;
+        amountCell.innerHTML = trades[i].amount;
+        leverageCell.innerHTML = trades[i].leverage;
+        entryPriceCell.innerHTML = trades[i].entryPrice;
+  
+        actionCell.innerHTML = '<button onclick="deleteRow(this)" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>';
+        refreshCell.innerHTML = '<button onclick="refreshRow(this)" id="rbtn" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i></button>';
+  
+        var coin = trades[i].coin;
+        var amount = trades[i].amount;
+        var entryPrice = trades[i].entryPrice;
+  
+        // Retrieve the current price of the coin from Coingecko API
+        var apiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" + coin + "&vs_currencies=usd";
+        var response = await fetch(apiUrl);
+        var data = await response.json();
+        var currentPrice = data[coin.toLowerCase()].usd;
+  
+        // Calculate the profit and profit rate
+        var profit = (currentPrice / entryPrice) * amount - amount;
+        var profitRate = (profit / amount) * 100;
+  
+        currentpriceCell.innerHTML = currentPrice;
+        profitCell.innerHTML = profit.toFixed(2);
+        profitRateCell.innerHTML = profitRate.toFixed(2) + "%";
+  
+        // Set the color of the profitRateCell based on the value
+        if (profitRate > 0) {
+          profitRateCell.style.color = "green";
+        } else if (profitRate < 0) {
+          profitRateCell.style.color = "red";
+        }
+  
+        if (profit > 0) {
+          profitCell.style.color = "green";
+        } else if (profit < 0) {
+          profitCell.style.color = "red";
+        }
+      }
+    }
+  }; 
+
   function clearTable() {
     var table = document.getElementById("trade-table");
   
