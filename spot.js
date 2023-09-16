3848e15417a1140f4cf2a42a4d46ed083ebf5a94
selamierdn <erdenselami07@gmail.com>
2023-09-16 22:25:04 +0300

    ok

--- spot.js
+++ spot.js
@@ -1,97 +1,91 @@
+function calculateProfit(yon, currentPrice, firstprice, miktar, leverage) {
+  const profit = yon === "Long" ?
+    ((currentPrice / firstprice) * miktar - miktar) * leverage :
+    -1 * (((currentPrice / firstprice) * miktar - miktar) * leverage);
 
-async function submitForm(){
+  const profitRate = (profit / miktar) * 100;
+  return { profit, profitRate };
+}
 
-  var yon = document.getElementById("longshort").value;
-  var coin = document.getElementById("coin").value;
-  var miktar = document.getElementById("miktar").value;
-  var firstprice = document.getElementById("firstprice").value;
-  var leverage = document.getElementById("kaldirac").value;
+function updateUIWithProfit(row, spot) {
+  const {
+    coin, miktar, firstprice, leverage, yon, currentPrice, profit, profitRate
+  } = spot;
 
-  if(leverage === ""){
-    leverage = 1;
-  }
-
-  
-  // Retrieve the current price of the coin from Coingecko API
-  var apiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" + coin + "&vs_currencies=usd";
-  var response = await fetch(apiUrl);
-  var data = await response.json();
-  var currentPrice = data[coin.toLowerCase()].usd;
-
-  if(yon === "Long"){
-    var profit = ((currentPrice / firstprice) * miktar - miktar) * leverage;
-    var profitRate = (profit / miktar) * 100;
-  } else {
-    var profit = -1 * (((currentPrice / firstprice) * miktar - miktar) * leverage);
-    var profitRate = (profit / miktar) * 100;
-  }
-
-  
-
-  
-  var spot = {
-    coin:coin,
-    miktar:miktar,
-    firstprice:firstprice,
-    leverage:leverage,
-    yon:yon,
-    currentPrice:currentPrice,
-    profit:profit,
-    profitRate:profitRate,
-  };
-  
-  if(localStorage.getItem("cspots") === null){
-    localStorage.setItem("cspots", "[]");
-  }
-  
-  var cspots = JSON.parse(localStorage.getItem("cspots"));
-  cspots.push(spot);
-  localStorage.setItem("cspots",JSON.stringify(cspots));
-  
-  var table = document.getElementById("table");
-  var row = table.insertRow(-1);
-  
-  var coinCell = row.insertCell(0);
-  var miktarCell = row.insertCell(1);
-  var firstpriceCell = row.insertCell(2);
-  var currentpriceCell = row.insertCell(3);
-  var leverageCell = row.insertCell(4);
-  var yonCell = row.insertCell(5)
-  var profitCell = row.insertCell(6);
-  var profitRateCell = row.insertCell(7);
-  var actionCell = row.insertCell(8);
-  var refreshCell = row.insertCell(9);
-  var finishCell = row.insertCell(10);
-
-  
-  coinCell.innerHTML = coin;
-  miktarCell.innerHTML = miktar;
-  firstpriceCell.innerHTML = firstprice;
-  leverageCell.innerHTML = leverage;
-  yonCell.innerHTML = yon;
-  currentpriceCell.innerHTML = currentPrice;
-  profitCell.innerHTML = profit.toFixed(2);
-  profitRateCell.innerHTML = profitRate.toFixed(2) + "%";
+  row.cells[0].innerHTML = coin;
+  row.cells[1].innerHTML = miktar;
+  row.cells[2].innerHTML = firstprice;
+  row.cells[3].innerHTML = currentPrice.toFixed(2);
+  row.cells[4].innerHTML = leverage;
+  row.cells[5].innerHTML = yon;
+  row.cells[6].innerHTML = profit.toFixed(2);
+  row.cells[7].innerHTML = profitRate.toFixed(2) + "%";
 
   if (profitRate > 0) {
-    profitRateCell.style.color = "green";
+    row.cells[7].style.color = "green";
   } else if (profitRate < 0) {
-    profitRateCell.style.color = "red";
+    row.cells[7].style.color = "red";
   }
 
   if (profit > 0) {
-    profitCell.style.color = "green";
-  } else if (profitRate < 0) {
-    profitCell.style.color = "red";
+    row.cells[6].style.color = "green";
+  } else if (profit < 0) {
+    row.cells[6].style.color = "red";
+  }
+}
+
+function addToLocalStorage(spot) {
+  const cspots = JSON.parse(localStorage.getItem("cspots")) || [];
+  cspots.push(spot);
+  localStorage.setItem("cspots", JSON.stringify(cspots));
+}
+
+async function submitForm() {
+  const yon = document.getElementById("longshort").value;
+  const coin = document.getElementById("coin").value;
+  const miktar = document.getElementById("miktar").value;
+  const firstprice = document.getElementById("firstprice").value;
+  let leverage = document.getElementById("kaldirac").value;
+
+  if (leverage === "") {
+    leverage = 1;
   }
 
-  actionCell.innerHTML = '<button onclick="deleteRow(this)" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>';
-  refreshCell.innerHTML = '<button onclick="refreshRow(this)" id="rbtn" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i></button>';
-  finishCell.innerHTML = '<button onclick="finishRow(this)" class="d-flex align-items-baseline btn btn-outline-success">Sold<i class="fas fa-check"></i></button>';
+  const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const currentPrice = data[coin.toLowerCase()].usd;
+
+  const { profit, profitRate } = calculateProfit(yon, currentPrice, firstprice, miktar, leverage);
+
+  const spot = {
+    coin,
+    miktar,
+    firstprice,
+    leverage,
+    yon,
+    currentPrice,
+    profit,
+    profitRate,
+  };
+
+  addToLocalStorage(spot);
+
+  const table = document.getElementById("table");
+  const row = table.insertRow(-1);
+
+  for (let i = 0; i < 11; i++) {
+    row.insertCell(i);
+  }
+
+  updateUIWithProfit(row, spot);
+
+  row.cells[8].innerHTML = '<button onclick="deleteRow(this)" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>';
+  row.cells[9].innerHTML = '<button onclick="refreshRow(this)" id="rbtn" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i></button>';
+  row.cells[10].innerHTML = '<button onclick="finishRow(this)" class="d-flex align-items-baseline btn btn-outline-success">Sold<i class="fas fa-check"></i></button>';
 
   updateBalance();
-};
-
+}
 
 async function refreshRow(button) {
   var yon = document.getElementById("longshort").value;
@@ -225,7 +219,6 @@ function finishRow(button) {
   }
 }
 
-
 var refreshButtons = document.querySelectorAll("#table button.btn-outline-primary");
 refreshButtons.forEach(button => {
   button.addEventListener("click", function() {
@@ -234,20 +227,13 @@ refreshButtons.forEach(button => {
 });
 
 function deleteRow(button) {
-  // Get the row that the button is in
+
   var row = button.parentNode.parentNode;
-  
-  // Get the spot data from the row cells
+
   var coin = row.cells[0].innerHTML;
-  var miktar = row.cells[1].innerHTML;
-  var firstprice = row.cells[2].innerHTML;
-  var leverage = row.cells[4].innerHTML;
-  var yon = row.cells[5].innerHTML;
-  
-  // Remove the row from the table
+
   row.parentNode.removeChild(row);
   
-  // Remove the spot data from localStorage
   var cspots = JSON.parse(localStorage.getItem("cspots"));
   var index = -1;
   for (var i = 0; i < cspots.length; i++) {
@@ -291,7 +277,6 @@ function updateBalance() {
 
 }
 
-
 function clearTable() {
   var table = document.getElementById("table");
 
@@ -303,7 +288,6 @@ function clearTable() {
 };
 
 window.onload = async function() {
-  // Get the spots data from localStorage and populate the table
   var cspots = JSON.parse(localStorage.getItem("cspots"));
   if (cspots !== null) {
     var table = document.getElementById("table");
